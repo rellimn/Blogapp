7cce0a8cf3bd596d5c864f73b70dd62f041b83ae
lcorneli <ludwig.cornelius@hs-osnabrueck.de>
2023-07-11 16:37:48 +0200

    test

--- src/main/java/de/hsos/ooadss23/blogapp/controller/ArtikelController.java
+++ src/main/java/de/hsos/ooadss23/blogapp/controller/ArtikelController.java
@@ -1,9 +1,6 @@
 package de.hsos.ooadss23.blogapp.controller;
 
-import de.hsos.ooadss23.blogapp.datenbank.Artikel;
-import de.hsos.ooadss23.blogapp.datenbank.ArtikelRepository;
-import de.hsos.ooadss23.blogapp.datenbank.Kommentar;
-import de.hsos.ooadss23.blogapp.datenbank.Text;
+import de.hsos.ooadss23.blogapp.datenbank.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -40,4 +37,28 @@ public class ArtikelController {
         this.artikelRepository.save(artikel);
         return "redirect:/artikel/" + artikelId;
     }
+
+
+    @RequestMapping(value="/{artikelId}/neueBewertung", method = RequestMethod.GET)
+    public String bewertungHinzufuegenForm(@PathVariable int artikelId, Model model) {
+        model.addAttribute("artikelId", artikelId);
+        return "artikel/neueBewertung";
+    }
+
+    @RequestMapping(value="/{artikelId}/neueBewertung", method = RequestMethod.POST)
+    public String bewertungHinzufuegenHandling(@PathVariable int artikelId, @RequestParam int sterne, Model model) {
+        NutzerSession nutzerSession = (NutzerSession) model.getAttribute("nutzerSession");
+        // NullPointerException kann nicht auftreten, da RequireLoginInterceptor sicherstellt, dass eine nutzerSession vorhanden ist
+
+        Artikel artikel = this.artikelRepository.findById(artikelId).orElseThrow();
+        Bewertung bewertung = new Bewertung(sterne,nutzerSession.getNutzer(),artikel);
+        artikel.addBewertungen(bewertung);
+        this.artikelRepository.save(artikel);
+
+
+        //hier fehlt was
+        return "redirect:/artikel/" + artikelId;
+    }
+
+
 }
